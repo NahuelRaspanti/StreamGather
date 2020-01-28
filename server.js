0cca5bad8e5c3c5a42f4d5c22574d7177c1acddd
Nahuel Raspanti <Nahuel.Raspanti@ey.com>
2020-01-28 19:52:08 -0300

    adsasd

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ if (process.env.NODE_ENV === "production") {
     // serve index.html if route is not recognized
     const path = require('path');
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
     });
 }
 
