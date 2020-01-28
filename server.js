097092ccc79748e3804296ab8fd9484573045296
Nahuel Raspanti <Nahuel.Raspanti@ey.com>
2020-01-28 18:04:31 -0300

    .

--- server.js
+++ server.js
@@ -223,12 +223,11 @@ app.get('/', (req, res) => {
   });
 
 if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-
     // serve production assets e.g. main.js if route exists
-    app.use(express.static(path.resolve(__dirname, "../client/build")));
+    app.use(express.static('client/build'));
 
     // serve index.html if route is not recognized
+    const path = require('path');
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
     });
