7cf2f833a005bc983f12f01c476dbc56008a1bb9
Nahuel Raspanti <nahuiraspanti@gmail.com>
2020-02-20 14:12:27 -0300

    Added links to drawer and styled with ellipsis for long game names

--- client/src/Components/DrawerStream.js
+++ client/src/Components/DrawerStream.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { List, Avatar } from '@material-ui/core';
+import { List, Avatar, Typography } from '@material-ui/core';
 
 const DrawerStream = ({stream}) => {
     return(
-    <ListItem button key={stream.id}>
+    <ListItem button key={stream.id} component = "a" href = {stream.url} target = "_blank">
         <ListItemIcon>
             <Avatar alt={"Avatar"} src={stream.avatar}></Avatar>
         </ListItemIcon>
-        <ListItemText primary={stream.streamerName} secondary = {stream.game} />
+        <ListItemText primary={stream.streamerName} secondary = {
+            <p style = {{margin: '0px', overflow: 'hidden', textOverflow: 'ellipsis'}} variant = "caption">{stream.game}</p>}/>
     </ListItem>
+
     )
 }
 
